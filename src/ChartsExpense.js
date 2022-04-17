5be675c665b710c21ef1dae094313ee7ccfa2fec
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-04-18 04:32:24 +0530

    Expense Tracker

--- src/ChartsExpense.js
+++ src/ChartsExpense.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Doughnut } from "react-chartjs-2";
+import Spinner from "./Spinner";
 import axios from "axios";
 
 
@@ -11,62 +12,78 @@ const ChartsExpense = () => {
   const chart = () => {
     let getAmount = [];
     let getTask = [];
-
-    axios.get("https://my-expense-tracker-project.herokuapp.com/api/getexpense")
-      .then(res => {
-        for (const dataObj of res.data) {
-          getAmount.push(parseInt(dataObj.Amount));
-          getTask.push(dataObj.Task);
-        }
-        setChartData({
-          labels: getTask,
-          datasets: [
-            {
-              label: "Amount",
-              data: getAmount,
-              backgroundColor: function (context) {
-                return palette[context.dataIndex % palette.length];
-              },
-              borderWidth: 1
-            }
-          ]
+    const [loading, setLoading] = useState(true);
+      
+    useEffect(() => {
+      const loadData = async () => {
+        await new Promise((r) => setTimeout(r, 5000));
+        setLoading((loading) => !loading);
+      };
+        
+      loadData();
+    }, []);
+      axios.get("https://my-expense-tracker-project.herokuapp.com/api/getexpense")
+        .then(res => {
+          for (const dataObj of res.data) {
+            getAmount.push(parseInt(dataObj.Amount));
+            getTask.push(dataObj.Task);
+          }
+          setChartData({
+            labels: getTask,
+            datasets: [
+              {
+                label: "Amount",
+                data: getAmount,
+                backgroundColor: function (context) {
+                  return palette[context.dataIndex % palette.length];
+                },
+                borderWidth: 1
+              }
+            ]
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    console.log(getTask, getAmount);
-  };
+      console.log(getTask, getAmount);
+    };
 
-  useEffect(() => {
-    chart();
-  }, []);
-  return (
-    <div className="App">
-      <div>
-        <Doughnut
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "Money Added", display: true },
-            scales: {
-              yAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }]
-            }
-          }}
-        />
-      </div>
-    </div>
-  );
+    useEffect(() => {
+      chart();
+    }, []);
+
+    if (loading) {
+		  return (<Spinner />);
+	  }
+
+    else{
+      return (
+        <div className="App">
+          <div>
+            <Doughnut
+              data={chartData}
+              options={{
+                responsive: true,
+                title: { text: "Money Added", display: true },
+                scales: {
+                  yAxes: [
+                    {
+                      gridLines: {
+                        display: false
+                      }
+                    }],
+                  xAxes: [
+                    {
+                      gridLines: {
+                        display: false
+                      }
+                    }]
+                }
+              }}
+            />
+          </div>
+        </div>
+      );
+  }
 };
 export default ChartsExpense;
--- src/ChartsIncome.js
+++ src/ChartsIncome.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Doughnut } from "react-chartjs-2";
+import Spinner from "./Spinner";
 import axios from "axios";
 
 
 const ChartsIncome = () => {
   const [chartData, setChartData] = useState({});
+  const [loading, setLoading] = useState(true);
+      
+    useEffect(() => {
+      const loadData = async () => {
+        await new Promise((r) => setTimeout(r, 5000));
+        setLoading((loading) => !loading);
+      };
+        
+      loadData();
+    }, []);
 
   var palette = ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"];
 
@@ -41,32 +52,37 @@ const ChartsIncome = () => {
   useEffect(() => {
     chart();
   }, []);
-  return (
-    <div className="App">
-      <div>
-        <Doughnut
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "Money Added", display: true },
-            scales: {
-              yAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false
-                  }
-                }]
-            }
-          }}
-        />
+  if (loading) {
+    return (<Spinner />);
+  }
+  else{
+    return (
+      <div className="App">
+        <div>
+          <Doughnut
+            data={chartData}
+            options={{
+              responsive: true,
+              title: { text: "Money Added", display: true },
+              scales: {
+                yAxes: [
+                  {
+                    gridLines: {
+                      display: false
+                    }
+                  }],
+                xAxes: [
+                  {
+                    gridLines: {
+                      display: false
+                    }
+                  }]
+              }
+            }}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 export default ChartsIncome;
