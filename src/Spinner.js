4f0729595a7f690c78d519e02cfa0f7cdc375220
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-04-18 04:06:37 +0530

    Expense Tracker

--- src/Spinner.js
+++ src/Spinner.js
@@ -5,7 +5,6 @@ function Spinner(){
     return (
         <div className='spinner'>
             <ReactLoading type="spin" color="rgba(138, 43, 226)" height={150} width={70} />
-            <h1 className='heading'> Loading Contents... </h1>
         </div>
     );
 }
