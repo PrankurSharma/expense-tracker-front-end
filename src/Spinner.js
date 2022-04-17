6cacb3a0b15057883eefd060dce5175120d950f5
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-04-18 03:56:56 +0530

    Expense Tracker

--- src/Spinner.js
+++ src/Spinner.js
@@ -4,7 +4,7 @@ import ReactLoading from 'react-loading';
 function Spinner(){
     return (
         <div className='spinner'>
-            <ReactLoading type="spin" color="rgba(138, 43, 226)" height={150} width={70} />
+            
             <h1 className='heading'> Loading Contents... </h1>
         </div>
     );
