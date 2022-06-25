c0e6086eea11a9cf77060f824ee7032e0b2ffd60
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-06-26 00:52:21 +0530

    Expense Tracker

--- src/ChartsExpense.js
+++ src/ChartsExpense.js
@@ -8,8 +8,9 @@ const ChartsExpense = () => {
 
   var palette = ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"];
 
-    var getAmount = [];
-    var getTask = [];
+  const chart = () => {
+    let getAmount = [];
+    let getTask = [];
 
     axios.get("https://my-expense-tracker-project.herokuapp.com/api/getexpense")
       .then(res => {
@@ -17,26 +18,36 @@ const ChartsExpense = () => {
           getAmount.push(parseInt(dataObj.Amount));
           getTask.push(dataObj.Task);
         }
+        setChartData({
+          labels: getTask,
+          datasets: [
+            {
+              label: "Amount",
+              data: getAmount,
+              backgroundColor: function (context) {
+                return palette[context.dataIndex % palette.length];
+              },
+              borderWidth: 1
+            }
+          ]
+        });
       })
       .catch(err => {
         alert(err);
       });
+  };
 
+  useEffect(() => {
+    chart();
+  }, []);
   return (
     <div className="App">
       <div style={{height:"auto",position:"relative", marginBottom:"1%", padding:"1%"}}>
         <Pie
-          data={{ 
-            labels: getTask,
-            datasets: [
-              {
-                data: getAmount,
-                borderWidth: 1
-              }
-            ]
-          }}
+          data={chartData}
           options={{
             responsive: true,
+            title: { text: "Money Added", display: true },
             plugins: {
               legend: {
                 display: false
