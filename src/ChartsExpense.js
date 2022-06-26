4ca68f6422d01de3999c557a23b27f48190709eb
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-06-26 23:38:57 +0530

    Expense Tracker

--- src/ChartsExpense.js
+++ src/ChartsExpense.js
@@ -45,7 +45,6 @@ const ChartsExpense = () => {
       <div style={{position: "relative", height:"40vh", width:"80vw"}}>
         <Doughnut
           data={chartData}
-          width="300%"
           options={{
             responsive: false,
             scales: {
