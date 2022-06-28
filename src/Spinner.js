cb6012ce2102b4f3d3214913ce7ec5a62accc92a
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-06-28 19:45:43 +0530

    Expense Tracker

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ReactLoading from 'react-loading';
 import Axios from "axios";
 
 function Spinner() {
     Axios.defaults.withCredentials = true;
+    
+    const navigate = useNavigate();
+    const navigateToLogin = () => {
+        navigate('login');
+    };
+    
     useEffect(() => {
         Axios.get('https://my-expense-tracker-project.herokuapp.com/api/login').then((response) => {
             if (!response.data[0].person_id && !response.data[0].username) {
-                window.location.href = '/login';
+                navigateToLogin();
             }
         })
     }, []);
