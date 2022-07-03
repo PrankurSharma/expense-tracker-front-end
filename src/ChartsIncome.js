16ef1da942cde770aa44623ab0960ea08a00eba5
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-07-04 02:10:56 +0530

    Expense Tracker

--- src/ChartsIncome.js
+++ src/ChartsIncome.js
@@ -39,7 +39,7 @@ const ChartsIncome = () => {
 
   useEffect(() => {
     chart();
-  }, [chartData]);
+  }, []);
   return (
     <div className="App">
       <div style={{width: '100%', height: '100%'}}>
